0cd19f1f3815e2ddf9cd65420fff246623f177ea
iamadawra <aayushgopaldawra@gmail.com>
2013-07-09 21:29:17 -0700

    Implemented Singly Linked List

--- toolbox.js
+++ toolbox.js
@@ -1,1 +1,19 @@
-//Initial Commit
+/*******************************
+Singly Linked List
+*******************************/
+
+function Node(){
+	this.content = null; 
+	this.next = null; //Null by default, can be changed later
+}
+
+//Create a Singly Linked list with a given number of nodes
+function createLinkedList(numberOfNodes){
+	var head = new Node();
+	var currNode = head;
+	for (var i = 1; i < numberOfNodes; i++){
+		var nextNode = new Node();
+		currNode.next = nextNode;
+		currNode = nextNode;
+	}
+}

5d04ff464695d5a2049605d5b049e2c6c6ec07ce
iamadawra <aayushgopaldawra@gmail.com>
2013-07-10 00:10:30 -0700

    Implemented Doubly Linked List

--- toolbox.js
+++ toolbox.js
@@ -7,13 +7,36 @@ function Node(){
 	this.next = null; //Null by default, can be changed later
 }
 
-//Create a Singly Linked list with a given number of nodes
-function createLinkedList(numberOfNodes){
-	var head = new Node();
+
+/*******************************
+Doubly Linked List
+*******************************/
+
+function doubleNode(){
+	this.content = null;
+	this.next = null; //Next Pointer
+	this.prev = null; //Previous Pointer
+}
+
+
+//Creates a singly(default) or doubly linked list. linkedlisttpye = Double for doubly linked list
+function createLinkedList(numberOfNodes, linkedListType){
+	if (linkedListType.toString() == "Double"){
+		var head = new doubleNode();
+	}else{
+		var head = new Node();
+	}
 	var currNode = head;
 	for (var i = 1; i < numberOfNodes; i++){
-		var nextNode = new Node();
-		currNode.next = nextNode;
-		currNode = nextNode;
+		if (linkedListType.toString() == "Double"){
+			var nextNode = new doubleNode();
+			currNode.next = nextNode;
+			nextNode.prev = currNode;
+			currNode = nextNode;
+		}else{
+			var nextNode = new Node();
+			currNode.next = nextNode;
+			currNode = nextNode;
+		}
 	}
 }
